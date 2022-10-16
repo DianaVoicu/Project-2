33a43e91c6b5555e25c57d6d11e8beb0619a5521
DianaVoicu <dianapop.pas@gmail.com>
2022-10-16 18:46:50 +0000

    Add Event Listeners to the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,37 @@ let myDate = document.getElementById('date');
 let today = (new Date()).toLocaleDateString('en-GB');
 myDate.innerHTML = today;
 
-let mySubmit = document.getElementById('submit');
-mySubmit.addEventListener('click', myList);
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName ('button');
 
-function myList () {
-        let myDiv = document.getElementById('text').textContent;
-        let newList = `
-                    <div class="list">
-                        <li>${myDiv}</li>
-                        <button id="check"><i class="fas fa-check"></i></button>
-                        <button id="edit"><i class="fas fa-edit"></i></button>
-                        <button id="trash"><i class="fas fa-trash-alt"></i></button>
-                    </div>
-        `;
-    
-        let myList = document.getElementsByTagName('ul')[0];
-        myList.appendChild(newList);   
+    for (let button of buttons) {
+        button.addEventListener ('click', function () {
+            if(this.id === 'add') {
+                addList ()
+            } else if (this.id === 'trash') {
+                alert('You clicked trash')
+            } else if (this.id === 'edit') {
+                alert('You clicked the edit')
+            } else {
+                alert ('You clicked done')
+                
+            }
+        });
     }
-    myList();
+});
 
+function addList () {
+    let newTask = document.getElementById('text').value;
+    let listItems =  `
+    <div class="list">
+        <li>${newTask}</li>
+        <button id="check"><i class="fas fa-check"></i></button>
+        <button id="edit"><i class="fas fa-edit"></i></button>
+        <button id="trash"><i class="fas fa-trash-alt"></i></button>
+    </div>`;
+    let myDiv = document.createElement ('div');
+    myDiv.innerHTML = listItems;
+    document.getElementsByTagName('ul')[0].appendChild(myDiv);
 
+}
 
