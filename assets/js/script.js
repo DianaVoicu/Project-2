b6f63bff8222d244668cb055d2dfb56c6503428f
DianaVoicu <dianapop.pas@gmail.com>
2022-10-18 07:53:49 +0000

    Add media screen for tablets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
+/* Date for today in the title of to-do app */
+
 let myDate = document.getElementById('date');
 let today = (new Date()).toLocaleDateString('en-GB');
 myDate.innerHTML = today;
 
+/* Click event to add the input text in the list with plans */
 
 let addButton = document.querySelector ('#add');   
 addButton.addEventListener ('click', addList);
-         
+
 function addList () {
     let newTask = document.getElementById('text').value;
     if (newTask === "") {
-        alert ('Ohh! You need to type a task for today!')
+        alert ("I can't make plans for you yet! Please type a task for your list!")
     } else {
         document.querySelector('.test').innerHTML += `
                 <div class="list">
@@ -19,25 +22,35 @@ function addList () {
                 </div>
         `;
 
+/* Reseating the value to empty string for the input text after the add button is pressed */
+
         document.querySelector('#text').value = '';
+
+/* Click event to remove the task with the trash button */
 
         let currentTask = document.querySelectorAll ('.trash');
             for (let i=0; i < currentTask.length; i++) {
-            currentTask[i].onclick = function () {
-                this.parentNode.remove ();
+                currentTask[i].onclick = function () {
+                    this.parentNode.remove ();
                 }
             }
+
+/* Click event to strike the task with a double blue line for check button */
 
         let check = document.querySelectorAll ('.check');
         for (let i=0; i < check.length; i++) {
             check[i].onclick = function () {
                 this.parentNode.style.textDecoration = 'line-through';
+                this.parentNode.style.textDecorationColor = 'blue';
+                this.parentNode.style.textDecorationStyle = 'double';
             }
         }
     }
 }
 
-document.querySelector('#text').addEventListener('keydown', function (event) {
+/* Event listener for enter key to perform the addList () */
+
+    document.querySelector('#text').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         addList ();
     }
